3225f4755b1d74a7f4b601ada2683ec4ae2bf790
Yufei Wang <Yufei-Mac@Yufeis-MacBook-Pro.local>
2016-05-18 22:30:33 -0700

    Add TapGesture to ImageView, and get tap location

--- ShanghaiSubway/ViewController.swift
+++ ShanghaiSubway/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    var locationRect = CGRect()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(_:)))
+        self.imageView.userInteractionEnabled = true
+        self.imageView.addGestureRecognizer(tapGestureRecognizer)
+        self.scrollView.minimumZoomScale = 1.0
+        self.scrollView.maximumZoomScale = 10.0
+        locationRect = CGRectMake(0, 20, 20, 40)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        print(imageView.center.x,imageView.center.y)
+        return self.imageView
+    }
+    
+    func handleTap(sender: UITapGestureRecognizer){
+        //CGPoint p
+        let touchPoint = sender.locationInView(self.imageView)
+        print(touchPoint.x,touchPoint.y)
+    }
+    
 }
 
